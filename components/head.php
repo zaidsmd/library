243e816b2d04d73e64d05aa5dcb0575189736228
Zaid Samadi <zaidsmd111@gmail.com>
2023-03-22 09:42:36 +0000

    fix ios preview

--- components/head.php
+++ components/head.php
@@ -4,3 +4,5 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 <link rel="icon" href="../pictures/logo.png" type="image/gif" sizes="16x16">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="../scripts/modernizr-custom.js" ></script>
